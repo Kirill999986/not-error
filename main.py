c08d2f9f80327e1bf5e42dcdfbeeca79c4cf71be
Kirill <kirill2010.dp@gmail.com>
2022-10-10 15:37:08 +0300

    Init

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 result = []
-kem =1
 def divider(a, b):
-  if a < b:
-   raise ValueError
-  if b > 100:
-    raise IndexError
+    if a < b:
+        raise ValueError
+    if b > 100:
+        raise IndexError
     return a/b
-    data = {10, 2, 2, 5, 123, 4, 18, 0, 15, 8, 4}
-    for key in data:
-      res = divider(key, data[kem])
-      result.append(res)
+
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, 8 : 4}
+for key in data:
+  try:
+    res = divider(key, data[key])
+    result.append(res)
+  except Exception as error:
+    print(f'Что-то пошло не так: {error}')
 
 print(result)
